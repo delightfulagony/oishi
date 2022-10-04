4ef6af96286c555acf5535cb4c8e713fad38b5f1
Francisco Jose Cotan <iscoct@correo.ugr.es>
2022-10-04 13:44:39 +0200

    Creada estructura Ingrediente
    Este commit cierra el issue #23

--- oishi/src/main.rs
+++ oishi/src/main.rs
@@ -1,3 +1,7 @@
+pub mod types;
+
+use crate::types::ingrediente::Ingrediente;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ oishi/src/types/ingrediente.rs
@@ -0,0 +1,11 @@
+pub struct Ingrediente {
+    nombre: String,
+    energia: u16,
+    grasas: f32,
+    grasas_saturadas: f32,
+    hidratos_de_carbono: f32,
+    azucares: f32,
+    fibra_alimentaria: f32,
+    proteinas: f32,
+    sal: f32
+}
