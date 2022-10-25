26be909d53e0937bca4c265f2e64465100925c8d
Francisco Jose Cotan <iscoct@correo.ugr.es>
2022-10-25 09:57:27 +0200

    Modificada estruct. ingrediente e info_nutricional
    Este commit cierra el issue #23
    
    Se crea el tipo info_nutricional para abstraer la informacion
    nutricional de cada ingrediente/alimento

--- oishi/src/types/ingrediente.rs
+++ oishi/src/types/ingrediente.rs
@@ -1,11 +1,6 @@
-pub struct Ingrediente {
-    nombre: String,
-    energia: u16,
-    grasas: f32,
-    grasas_saturadas: f32,
-    hidratos_de_carbono: f32,
-    azucares: f32,
-    fibra_alimentaria: f32,
-    proteinas: f32,
-    sal: f32
+use crate::types::info_nutricional::InfoNutricional;
+
+pub(crate) struct Ingrediente {
+    pub(crate) nombre: String,
+    pub(crate) info_nutricional: InfoNutricional,
 }
