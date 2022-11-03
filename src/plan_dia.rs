7d70a2e30680e6e303f46443bf71fa06c5777a82
agony <agony@disroot.org>
2022-11-03 10:04:10 +0100

    Cambia el nombre de MenuDia a PlanDia
    Se cambia el nombre pues menú da la impresión de ser 1 comida,
    en lugar de todas las comidas del día.

--- src/plan_dia.rs
+++ src/plan_dia.rs
@@ -1,6 +1,6 @@
 use crate::receta::Receta;
 
-pub(crate) struct MenuDia {
+pub(crate) struct PlanDia {
     pub(crate) desayuno: Vec<Receta>,
     pub(crate) almuerzo: Vec<Receta>,
     pub(crate) cena: Vec<Receta>,
